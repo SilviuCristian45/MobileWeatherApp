65f0979a215d6753fd7af066958e9cd50a3c8238
Silviu <silviudinca412@gmail.com>
2023-02-04 17:42:30 +0200

    initial functionality

---
+++ App.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [city, setCity] = useState('')
+  const [temperature, setTemperature] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      loadData(location.coords.latitude, location.coords.longitude)
+    })();
+  }, []);
+
+  async function loadData(lat, lon) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=878f7ebe7c17602e944d2532f5b4d96d&units=metric`
+    const response = await (await axios.get(url)).data
+    setCity(response.name)
+    setTemperature(response.main.temp_max)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text> Your city is {city} </Text>
+      <Text> Your city is {temperature} </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'red'
+  }
+});
+  
