ef5c6cc774e06ec4696573be816afe8818057d4f
Silviu <silviudinca412@gmail.com>
2023-03-10 23:06:36 +0200

    added some icons and increase font size

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import * as Location from 'expo-location';
 import axios from 'axios';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {API_KEY} from '@env'
 
 export default function App() {
@@ -28,17 +32,40 @@ export default function App() {
   async function loadData(lat, lon) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     const response = await (await axios.get(url)).data
+    console.log(response)
     setCity(response.name)
     setTemperature(response.main.temp_max)
   }
 
   return (
     <View style={styles.container}>
-      <Text> Your city is {city} </Text>
-      <Text> Your city is {temperature} </Text>
+      <Text style={{ fontSize: 40 }} > {city} </Text>
+      <View style={styles.inline}>
+        <Text style={{ fontSize: 40 }}> {temperature} </Text>
+        <MaterialCommunityIcons name="temperature-celsius" size={40} color="black" />
+        { weatherIcon("Clouds") }
+      </View>
+      
+     
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function weatherIcon(weatherType) {
+  switch (weatherType) {
+    case "Clouds":
+      return (
+        <AntDesign name="cloud" size={48} color="black" />
+      )
+    case "Rain":
+      return (
+        <FontAwesome5 name="cloud-rain" size={24} color="black" />
+      )
+  
+    default:
+      break;
+  }
 }
 
 const styles = StyleSheet.create({
@@ -50,6 +77,11 @@ const styles = StyleSheet.create({
   },
   title: {
     color: 'red'
+  },
+  inline: {
+    display: 'flex',
+    flexDirection: 'row'
   }
 });
+
   
